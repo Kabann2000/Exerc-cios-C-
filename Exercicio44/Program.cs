c9ca0c18566804fdee78d1892b38e7bb9252283b
Kabann2000 <josericardocosta@hotmail.com>
2024-08-29 19:11:34 -0300

    Add files via upload

---
+++ Exercicio44/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ 44) O programa acima vai ter um problema quando digitarmos o primeiro valor
+maior que o último. Resolva esse problema com um código que funcione em qualquer
+situação.
+ */
+
+int primeiroValor, segundoValor, incremento;
+
+Console.WriteLine("Digite o primeiro valor:");
+primeiroValor = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo valor:");
+segundoValor = Convert.ToInt32(Console.ReadLine());
+
+while (primeiroValor > segundoValor)
+{
+    Console.WriteLine("O primeiro valor deveria ser menor que o segundo");
+    Console.WriteLine("Digite o primeiro valor:");
+    primeiroValor = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor:");
+    segundoValor = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Digite o incremento:");
+incremento = Convert.ToInt32(Console.ReadLine());
+
+while (primeiroValor <= segundoValor)
+{
+    Console.Write(primeiroValor + " ");
+
+    primeiroValor += incremento;
+}
+
+Console.Write("Acabou!");
