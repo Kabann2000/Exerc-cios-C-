55c064b6a799833c161e6562746864245d48a249
Kabann2000 <josericardocosta@hotmail.com>
2024-08-29 19:19:33 -0300

    Add files via upload

---
+++ Exercicio62/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+62) Escreva um programa que leia um número qualquer e mostre a tabuada desse
+número, usando a estrutura “para”.
+Ex: Digite um valor: 5
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15 ...
+*/
+
+int multiplicando;
+
+Console.WriteLine("Digite um número da tabuada:");
+multiplicando = Convert.ToInt32(Console.ReadLine());
+
+for(int contador = 1; contador <= 10; contador++)
+{
+    Console.WriteLine(multiplicando + " x " + contador + " = " + (multiplicando * contador));
+}
